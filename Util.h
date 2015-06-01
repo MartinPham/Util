490e72d4b8b8e289ccfed489b987bed68d7e94d3
Martin Pham <tuyentq2009@gmail.com>
2015-06-01 22:32:08 +0700

    ok

--- Util.h
+++ Util.h
@@ -47,28 +47,6 @@ typedef enum __UIImageGradientDirection
 @end
 
 
-@interface NSString (MD5)
-- (NSString *)md5;
-@end
-
-#import <CommonCrypto/CommonDigest.h> // Need to import for CC_MD5 access
-
-@implementation NSString (MyExtensions)
-- (NSString *)md5
-{
-    const char *cStr = [self UTF8String];
-    unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), result ); // This is the md5 call
-    return [NSString stringWithFormat:
-            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-            result[0], result[1], result[2], result[3],
-            result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11],
-            result[12], result[13], result[14], result[15]
-            ];
-    
-}
-@end
 
 #define ASYNC_LOAD_IMG(img, src) img.image = nil;[Util loadData:src cache:YES callback:^(NSData *data){ [img setImage:[UIImage imageWithData:data]];}];
 
